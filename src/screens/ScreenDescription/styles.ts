28f268c6a727d7955c71b2ba17f08ea63044e0d1
ViniD7 <114617536+ViniD7@users.noreply.github.com>
2023-10-29 01:51:29 -0300

    ScreenDescription

--- src/screens/ScreenDescription/styles.ts
+++ src/screens/ScreenDescription/styles.ts
@@ -4,26 +4,30 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(11, 11, 22)',
+        paddingTop: 50,
+        paddingHorizontal: 20,
+    },
+    descriptionContainer: {
+        marginBottom: 20,
+        backgroundColor: 'rgba(255,255,255,0.1)',
+        minHeight: 280,
+        borderRadius: 20,
+        padding: 15,
+        paddingTop: 20,
         justifyContent: 'space-between',
     },
-
-    task: {
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginVertical: 20,
+        color: '#fff',
+    },
+    description: {
         color: 'white',
-        fontSize: 22,
-        fontWeight: '500',
-        marginRight: 5
-    },
-    organizeTask: {
-        paddingHorizontal: 15,
-        paddingTop: 40,
-        paddingBottom: 20,
-        height: '60%',
-    },
-    titlePending: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 50
-    },
+        fontSize: 18,
+        fontWeight: '500'
+    }
 
 });
 
